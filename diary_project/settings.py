5c0968abf228db7057022c46f11bed60d0fabcf9
reshmarajeev12 <reshmarajeevkumar123@gmail.com>
2022-03-02 15:48:33 +0530

    page created

--- diary_project/settings.py
+++ diary_project/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+#own
+'dairy_app.apps.DairyAppConfig',
+'crispy_forms',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
