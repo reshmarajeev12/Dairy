550df0bbe327ca6fe25c4bdb8e0b26fa081e6da3
reshmarajeev12 <reshmarajeevkumar123@gmail.com>
2022-03-07 20:14:37 +0530

    add entry created

--- diary_project/settings.py
+++ diary_project/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname ( os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,10 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-#own
-'dairy_app.apps.DairyAppConfig',
-'crispy_forms',
+    'entries'
 ]
 
 
@@ -81,7 +79,7 @@ WSGI_APPLICATION = 'diary_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME':os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
