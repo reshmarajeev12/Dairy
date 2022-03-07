e1f3805a928355d3c28102c825b6d30619e0c980
reshmarajeev12 <reshmarajeevkumar123@gmail.com>
2022-03-07 23:28:00 +0530

    what's in your mind

--- entries/views.py
+++ entries/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render 
+from multiprocessing import context
+from django.shortcuts import render, redirect
+from .models import Entry 
+from .forms import EntryForm
 
 def index(request):
-    return render(request, 'entries/index.html')
+    entries = Entry.objects.order_by('-date_posted')
+
+    context = {'entries' : entries}
+
+    return render(request, 'entries/index.html', context)
 
 def add(request):
-    return render(request, 'entries/add.html')
+    if request.method == 'POST':
+        form = EntryForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+
+
+      form = EntryForm()
+
+    context = {'form' : form}
+
+    return render(request, 'entries/add.html', context)
